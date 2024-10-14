330eec1fc53324b60fa69869591e6264fd086fc3
kelvinpraises <kelvinpraises@gmail.com>
2024-10-14 23:33:57 +0100

    feat: add home page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/organisms/footer";
+import HeroPage from "@/components/organisms/hero-page";
+import StickyScrollPage from "@/components/organisms/sticky-scroll-page";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroPage />
+      <StickyScrollPage />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
