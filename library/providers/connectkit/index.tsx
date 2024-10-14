991d6759717cff591f22c4c24a7036b664944e06
kelvinpraises <kelvinpraises@gmail.com>
2024-10-14 14:50:37 +0100

    chore: remove unused

--- library/providers/connectkit/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ConnectKitProvider as _ConnectKitProvider } from "connectkit";
-
-const ConnectKitProvider = ({ children }: { children: React.ReactNode }) => {
-  return <_ConnectKitProvider>{children}</_ConnectKitProvider>;
-};
-
-export default ConnectKitProvider;
--- library/providers/wagmi/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { WagmiProvider as _WagmiProvider } from "wagmi";
-import { config } from "./config";
-
-const WagmiProvider = ({ children }: { children: React.ReactNode }) => {
-  return <_WagmiProvider config={config}>{children}</_WagmiProvider>;
-};
-
-export default WagmiProvider;
