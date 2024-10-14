6745619a03aa354fef2e65c2a8d620e8b772a1f8
kelvinpraises <kelvinpraises@gmail.com>
2024-10-14 14:50:52 +0100

    chore: clean up providers

--- library/providers/index.tsx
+++ library/providers/index.tsx
@@ -1,27 +1,7 @@
-"use client";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-import ConnectKitProvider from "./connectkit";
-import SIWEProvider from "./siwe";
-import WagmiProvider from "./wagmi";
-import XMTPProvider from "./xmtp";
-
-const queryClient = new QueryClient();
-
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <WagmiProvider>
-      <QueryClientProvider client={queryClient}>
-        <SIWEProvider>
-          <ConnectKitProvider>
-            <XMTPProvider>{children}</XMTPProvider>
-          </ConnectKitProvider>
-        </SIWEProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+  return <>{children}</>;
 };
 
 export default RootProvider;
