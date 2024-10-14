f2456e57bcdd6ede0c041f705c2c86542a89dfaf
kelvinpraises <kelvinpraises@gmail.com>
2024-10-14 23:40:35 +0100

    feat: add matched paths

--- library/components/template/layout-wrapper.tsx
+++ library/components/template/layout-wrapper.tsx
@@ -2,10 +2,12 @@
 
 import { Inter, Outfit } from "next/font/google";
 import localFont from "next/font/local";
+import { usePathname } from "next/navigation";
 
 import { Toaster } from "@/components/atoms/sonner";
 import RootProvider from "@/providers";
 import { cn } from "@/utils";
+import Navbar from "../organisms/nav-bar";
 
 const inter = Inter({ subsets: ["latin"], preload: true });
 const outfit = Outfit({
@@ -18,7 +20,7 @@ const outfit = Outfit({
 const atyp = localFont({
   src: [
     {
-      path: "../public/fonts/AtypDisplay-Semibold.woff2",
+      path: "../../../public/fonts/AtypDisplay-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
@@ -28,10 +30,30 @@ const atyp = localFont({
 });
 
 const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  // Check if the current pathname matches any of the path patterns
+  // Define an array of path patterns
+  // e.g /^\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/somewhere\/[a-zA-Z0-9_-]+$/
+  const pathPatterns = [/^\/console+$/, /^\/console\/.*$/, /^\/redirect+$/];
+
+  const isPathMatched = pathPatterns.some((pattern) => pattern.test(pathname));
+
   return (
     <html lang="en">
       <body className={cn(inter.className, outfit.variable, atyp.variable)}>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <>
+            {isPathMatched ? (
+              <>{children}</>
+            ) : (
+              <>
+                <Navbar />
+                {children}
+              </>
+            )}
+          </>
+        </RootProvider>
         <Toaster />
       </body>
     </html>
