74c1985b556e31ac0a2068b0128f8233363a16ce
kelvinpraises <kelvinpraises@gmail.com>
2024-10-18 16:03:02 +0100

    feat: integrate keyless login

--- library/components/organisms/nav-bar.tsx
+++ library/components/organisms/nav-bar.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
+import SignInWithKeyless from "@/components/molecules/siwk-button";
+
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-6 h-[80px] z-[99999]">
       <Image src={"/logo.svg"} alt={"logo"} width={"190"} height={"3"} />
-      <button className=" font-outfit font-[16px] bg-black text-[white] px-6 rounded-[32px] py-3.5">
-        Connect Wallet
-      </button>
+
+      <SignInWithKeyless />
     </div>
   );
 };
