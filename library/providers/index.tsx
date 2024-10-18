ae2c6b4237c8e7aa55e2d71f10f21c19f24306b7
kelvinpraises <kelvinpraises@gmail.com>
2024-10-18 15:51:31 +0100

    feat: use keyless and surf providers

--- library/providers/index.tsx
+++ library/providers/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { AptosKeylessProvider } from "./keyless";
+import { AptosSurfProvider } from "./surf";
+
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <AptosKeylessProvider>
+      <AptosSurfProvider>{children}</AptosSurfProvider>
+    </AptosKeylessProvider>
+  );
 };
 
 export default RootProvider;
