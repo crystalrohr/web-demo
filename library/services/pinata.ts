9c803b4a885bf0df74e73dca7f63d16a9050c013
kelvinpraises <kelvinpraises@gmail.com>
2024-10-19 23:40:40 +0100

    feat: add a custom pinata factory

--- library/services/pinata.ts
+++ library/services/pinata.ts
@@ -1,6 +1,10 @@
 import { PinataSDK } from "pinata";
 
-export const pinata = new PinataSDK({
-  pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
-});
+export const createPinataSDK = (customJwt?: string) => {
+  return new PinataSDK({
+    pinataJwt: customJwt || `${process.env.PINATA_JWT}`,
+    pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
+  });
+};
+
+export const pinata = createPinataSDK();
