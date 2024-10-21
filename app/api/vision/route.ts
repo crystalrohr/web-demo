f653c2ddfaa8d730af33db23b246d227c48f97e2
kelvinpraises <kelvinpraises@gmail.com>
2024-10-21 20:30:37 +0100

    feat: add livepeer ai

--- app/api/vision/route.ts
+++ app/api/vision/route.ts
@@ -1,5 +1,6 @@
 import Anthropic from "@anthropic-ai/sdk";
 import { ImageBlockParam } from "@anthropic-ai/sdk/resources/index.mjs";
+import { Livepeer } from "@livepeer/ai";
 import { type NextRequest, NextResponse } from "next/server";
 
 import { pinata } from "@/services/pinata";
@@ -14,21 +15,47 @@ type SupportedImageType = (typeof SUPPORTED_IMAGE_TYPES)[number];
 
 export const dynamic = "force-dynamic";
 
+// TODO: if audio errors move on...
 export async function POST(req: NextRequest) {
   try {
-    const anthropic = new Anthropic();
-    const { cids } = await req.json();
+    const formData = await req.formData();
+    const capturedImages = formData.getAll("capturedImages");
+    const extractedAudio = formData.get("extractedAudio") as Blob | null;
 
-    if (!Array.isArray(cids) || cids.length === 0) {
+    if (!extractedAudio || !(extractedAudio instanceof File)) {
+      return NextResponse.json(
+        { error: "Invalid or missing audio data" },
+        { status: 400 }
+      );
+    }
+
+    if (!Array.isArray(capturedImages) || capturedImages.length === 0) {
       return NextResponse.json(
         { error: "Invalid or empty CIDs array" },
         { status: 400 }
       );
     }
 
+    const livepeer = new Livepeer({
+      httpBearer: "c98baf31-5203-4b42-9b50-578a16342201",
+    });
+
+    const anthropic = new Anthropic();
+
+    const result = await livepeer.generate.audioToText({
+      audio: extractedAudio,
+      modelId: "openai/whisper-large-v3",
+    });
+    const audioTranscription = result.textResponse?.text;
+
     const imageContents: ImageBlockParam[] = (
       await Promise.all(
-        cids.map(async (cid) => {
+        capturedImages.map(async (cid) => {
+          if (typeof cid !== "string") {
+            console.log(`Skipping non-string CID: ${cid}`);
+            return null;
+          }
+
           const file = await pinata.gateways.get(cid);
 
           if (
@@ -79,15 +106,36 @@ export async function POST(req: NextRequest) {
     const message = await anthropic.messages.create({
       model: "claude-3-5-sonnet-20240620",
       max_tokens: 1024,
+      system:
+        "You are an AI assistant tasked with creating concise, visually impaired-friendly descriptions for a series of video stills, considering both the visual content and associated audio context.",
       messages: [
         {
           role: "user",
-          content: imageContents,
+          content: [
+            ...imageContents,
+            {
+              type: "text",
+              text: `Audio transcription: "${audioTranscription}"
+
+                Analyze each image sequentially, considering both the visual content and the provided audio transcription. Create a brief, one-sentence description for each image that summarizes the key visual aspects and incorporates relevant audio context.
+
+                Provide your output as a JSON array of strings, with each string being a concise description of one image. Describe directly without using phrases like 'this image shows'. Ensure your descriptions are clear, specific, and accessible to visually impaired individuals, integrating audio context where relevant.
+              `,
+            },
+          ],
         },
       ],
     });
 
-    return NextResponse.json(message, { status: 200 });
+    if (message.content[0].type === "text") {
+      const upload = await pinata.upload.json(message);
+      return NextResponse.json({ cid: upload.cid }, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { error: "Unexpected response format" },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json(
