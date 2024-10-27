7f9aa36bbf15120bbbc8ce5bf33ca0b1807ac005
kelvinpraises <kelvinpraises@gmail.com>
2024-10-27 21:27:11 +0100

    Clear git cache

--- library/types/index.ts
+++ library/types/index.ts
@@ -1,3 +1,28 @@
+export enum ChainType {
+  APTOS = "APTOS",
+  ALGORAND = "ALGORAND",
+  EVM = "EVM", // For Ethereum and other EVM chains
+}
+
+export interface IProtocolService {
+  // Core staking functions
+  joinPool(): Promise<any>;
+  stake(amount: number): Promise<any>;
+  unstake(amount: number): Promise<any>;
+  claimRewards(): Promise<any>;
+
+  // Video caption related functions
+  captionVideo(ipfsHash: string): Promise<any>;
+  completeCaptionVideo(caption: string): Promise<any>;
+  getVideoCaptionsByHash(ipfsHash: string): Promise<any>;
+  getLastVideoCaption(userAddress: `0x${string}`): Promise<any>;
+  getIncompleteVideoCaptionTasks(): Promise<any>;
+
+  // Utility functions
+  validStaker(nodeAddress: `0x${string}`): Promise<boolean>;
+  stakedAmount(): Promise<number>;
+}
+
 export type VideoCaptionEvent = {
   user_address: string;
   ipfs_hash: string;
