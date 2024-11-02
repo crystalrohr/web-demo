71a6aa1fbb0ef528a5e1d7d754909add14af63e7
kelvinpraises <kelvinpraises@gmail.com>
2024-11-02 17:44:08 +0100

    chore: remove text color

--- app/console/page.tsx
+++ app/console/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className="flex flex-col justify-between items-center flex-1 overflow-scroll w-full">
       <div className="flex flex-col justify-start items-center gap-16">
         <div className="flex flex-col justify-center items-center gap-1 p-0">
-          <p className="font-atyp text-[64px] text-[#02071E] text-balance w-[15ch] leading-tight text-center">
+          <p className="font-atyp text-[64px] text-balance w-[15ch] leading-tight text-center">
             Start your node and earn rohr rewards!
           </p>
           <p className="font-medium text-sm leading-[17px] text-[#484E62]">
