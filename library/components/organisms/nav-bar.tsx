a6929498148c2d4c84d5cea9eef292cfd9f7eed0
kelvinpraises <kelvinpraises@gmail.com>
2024-11-02 17:42:03 +0100

    feat: add root connector

--- library/components/organisms/nav-bar.tsx
+++ library/components/organisms/nav-bar.tsx
@@ -1,13 +1,14 @@
+import Connect from "@/components/molecules/connect";
 import Image from "next/image";
-
-import SignInWithKeyless from "@/components/molecules/siwk-button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between px-6 h-[80px] z-[99999]">
-      <Image src={"/logo.svg"} alt={"logo"} width={"190"} height={"3"} />
-
-      <SignInWithKeyless />
+    <div className="flex items-center justify-between px-6 h-[80px] z-[99999] bg-white">
+      <Link href={"/"}>
+        <Image src={"/logo.svg"} alt={"logo"} width={"190"} height={"3"} />
+      </Link>
+      <Connect />
     </div>
   );
 };
