d203fad9f84bedafec3dce97a084da8b2d295002
kelvinpraises <kelvinpraises@gmail.com>
2024-11-02 17:43:56 +0100

    feat: add background dot

--- app/console/layout.tsx
+++ app/console/layout.tsx
@@ -4,8 +4,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex w-full flex-col items-center gap-8 pt-8 h-screen ">
-      <img src="/logo-vertical.svg" alt="logo" />
+    <div className="flex w-full flex-col items-center gap-8 pt-8 h-screen dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <img className="dark:invert" src="/logo-vertical.svg" alt="logo" />
       {children}
     </div>
   );
