05c2edf9eef29d26ce80e9cb60f48ee9c643d542
kelvinpraises <kelvinpraises@gmail.com>
2024-11-05 13:33:58 +0100

    feat: update connect and connector components with chain connector info

--- library/components/molecules/connect.tsx
+++ library/components/molecules/connect.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import { Button } from "@/components/atoms/button";
 import AdaptiveModal from "@/components/molecules/adaptive-modal";
-import { NetworkModalContext } from "@/types";
-import NetworkSelect, { NetworkIds } from "./network-select";
+import NetworkSelect, {
+  NetworkId,
+  NetworkIds,
+} from "@/components/molecules/network-select";
+import useStore from "@/store";
+import { ConnectorId, NetworkModalContext } from "@/types";
+import SignInWithKeyless from "./connector-aptos";
+import { WagmiConnection, WagmiConnector } from "./connector-evm";
 
-const ConfirmPage = ({
+const Connector = ({
   context,
   onNavigate,
   onClose,
@@ -13,47 +19,128 @@ const ConfirmPage = ({
   context: NetworkModalContext;
   onNavigate: (pageId: string) => void;
   onClose: () => void;
-}) => (
-  <div>
-    <p>Confirm connection to {context.network}?</p>
-    <Button
-      onClick={() => {
-        onClose();
-        onNavigate("select");
-      }}
-    >
-      Confirm
-    </Button>
-  </div>
-);
+}) => {
+  const evm: NetworkId[] = [
+    NetworkIds.ethereum,
+    NetworkIds.sepolia,
+    NetworkIds["base-sepolia"],
+  ];
 
-const Connect = () => {
-  const [toNetwork, setToNetwork] = useState<NetworkModalContext>({
-  } as NetworkModalContext);
+  if (evm.includes(context.network)) {
+    return (
+      <div className="flex flex-col p-4 gap-4">
+        <p>
+          {context.network
+            .split("-")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ")}
+        </p>
+
+        <WagmiConnector key={context.network} network={context.network} />
+
+        <Button
+          onClick={() => {
+            onNavigate("select");
+          }}
+        >
+          Back
+        </Button>
+      </div>
+    );
+  }
+
+  if (context.network === NetworkIds.aptos) {
+    return (
+      <div className="flex flex-col p-4 gap-4">
+        <p>Aptos</p>
+        <SignInWithKeyless />
+        <Button onClick={() => onNavigate("select")}>Back</Button>
+      </div>
+    );
+  }
 
   return (
-    <AdaptiveModal
-      trigger={
-        <button className="font-outfit font-[16px] bg-black text-[white] px-6 rounded-[32px] py-3.5">
-          {toNetwork.network ? toNetwork.network : "Connect"}
-        </button>
-      }
-      initialPage="select"
-      context={toNetwork}
-      onContextChange={(newContext) => setToNetwork(newContext)}
-      pages={[
-        {
-          id: "select",
-          title: "Select Network",
-          component: NetworkSelect,
-        },
-        {
-          id: "confirm",
-          title: "Confirm Connection",
-          component: ConfirmPage,
-        },
-      ]}
-    />
+    <div className="flex flex-col p-4 gap-4">
+      <p>
+        Sorry{" "}
+        {context.network
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")}{" "}
+        is not supported yet
+      </p>
+      <Button onClick={() => onNavigate("select")}>
+        Select another network
+      </Button>
+    </div>
+  );
+};
+
+type ConnectionHandlers = {
+  [key in ConnectorId]: ({ network }: { network: NetworkId }) => JSX.Element;
+};
+
+const connectionHandlers: ConnectionHandlers = {
+  wagmi: ({ network }: { network: NetworkId }) => (
+    <WagmiConnection network={network} />
+  ),
+  keyless: () => <SignInWithKeyless />,
+  zkLogin: () => <SignInWithKeyless />,
+  petra: () => <SignInWithKeyless />,
+};
+
+const Connect = () => {
+  const { currentConnection } = useStore();
+  const { network, connectorId } = currentConnection;
+
+  const [networkContext, setNetworkContext] = useState<NetworkModalContext>({
+    ignoredNetworks: [NetworkIds.ethereum],
+  } as NetworkModalContext);
+
+  const Connection = useMemo(
+    () =>
+      connectorId && connectionHandlers[connectorId]
+        ? connectionHandlers[connectorId]
+        : () => null,
+    [connectorId]
+  );
+
+  const handleContextChange = useCallback(
+    (newContext: NetworkModalContext) => setNetworkContext(newContext),
+    []
+  );
+
+  return (
+    <div className="flex gap-4">
+      {network && <Connection network={network} />}
+      <AdaptiveModal
+        trigger={
+          <button className="font-outfit font-[16px] bg-black text-[white] px-6 rounded-[32px] py-3.5">
+            {network
+              ? network
+                  .split("-")
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(" ")
+              : "Connect"}
+          </button>
+        }
+        initialPage="select"
+        context={networkContext}
+        onContextChange={handleContextChange}
+        pages={[
+          {
+            id: "select",
+            title: "Select Network",
+            component: NetworkSelect,
+          },
+          {
+            id: "confirm",
+            title: "Confirm Connection",
+            component: Connector,
+          },
+        ]}
+      />
+    </div>
   );
 };
 
