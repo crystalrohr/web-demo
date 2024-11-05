631ec43c6f4875f48203f3e0d2351452229415b7
kelvinpraises <kelvinpraises@gmail.com>
2024-11-05 13:28:15 +0100

    feat: add sepolia to config

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,15 +1,18 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http, webSocket } from "wagmi";
-import { anvil, baseSepolia, bscTestnet } from "wagmi/chains";
+import { anvil, sepolia, baseSepolia, bscTestnet } from "wagmi/chains";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "Farlensflow",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [baseSepolia, bscTestnet, anvil],
+    chains: [sepolia, baseSepolia, bscTestnet, anvil],
     multiInjectedProviderDiscovery: true,
     transports: {
+      [sepolia.id]: http(
+        `https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+      ),
       [baseSepolia.id]: http(
         `https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
       ),
