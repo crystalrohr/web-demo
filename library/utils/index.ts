f022f1b08d2ac768993447fe4d217b352db2179e
kelvinpraises <kelvinpraises@gmail.com>
2024-12-31 17:54:26 +0100

    chore: enable pascal case on format network

--- library/utils/index.ts
+++ library/utils/index.ts
@@ -106,6 +106,6 @@ export function formatNetworkName(network: string | null): string {
   }
   return network
     .split("-")
-    .map((word) => word.toLowerCase())
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
